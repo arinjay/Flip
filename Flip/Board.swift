bb78a975e77c2d6109e7873249874f13eb71d31a
arinjay <arinjaysharma421@gmail.com>
2017-03-26 00:35:13 +0530

    creating model of game

---
+++ Flip/Board.swift
@@ -0,0 +1,16 @@
+//
+//  Board.swift
+//  Flip
+//
+//  Created by Arinjay Sharma on 3/26/17.
+//  Copyright © 2017 Arinjay Sharma. All rights reserved.
+//
+
+import UIKit
+
+class Board: NSObject {
+
+    static let size = 8
+    var rows = [[StoneColor]]()
+    
+}
--- Flip/GameScene.swift
+++ Flip/GameScene.swift
@@ -12,6 +12,8 @@ import GameplayKit
 class GameScene: SKScene {
      var rows = [[Stone]]()
     
+    var board : Board!
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
@@ -28,16 +30,21 @@ class GameScene: SKScene {
         gameboard.zPosition = 2
         addChild(gameboard)
         
+        
+        
+        
+        board = Board()
+        
         // Setting up constant for positioning
         let offsetX = -280
         let offsetY = -281
         let StoneSize = 80
         
-        for row in 0 ..< 8 {
+        for row in 0 ..< Board.size {
             
             var colArray = [Stone]()
             
-            for col in 0 ..< 8{
+            for col in 0 ..< Board.size{
                 let stone = Stone(color: UIColor.clear,size: CGSize(width: StoneSize, height: StoneSize ))
             
                 stone.position = CGPoint(x: offsetX + (col * StoneSize), y: offsetY + (col * StoneSize))
@@ -48,6 +55,8 @@ class GameScene: SKScene {
                 gameboard.addChild(stone)
                 colArray.append(stone)
             }
+            
+            board.rows.append([StoneColor](repeating: .empty, count: Board.size))
             rows.append(colArray)
         }
         
@@ -56,7 +65,12 @@ class GameScene: SKScene {
         rows[3][4].setPlayer(.white)
         rows[3][3].setPlayer(.black)
         
-    }
+        
+        board.rows[4][3] = .white
+        board.rows[4][4] = .black
+        board.rows[3][4] = .white
+        board.rows[3][3] = .black
+     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
