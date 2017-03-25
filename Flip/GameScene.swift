39aa8b0b0eee9e23da5706bd69dc29ee0e831fcf
arinjay <arinjaysharma421@gmail.com>
2017-03-26 00:10:13 +0530

    Adding board & Stones

--- Flip/GameScene.swift
+++ Flip/GameScene.swift
@@ -10,6 +10,7 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+     var rows = [[Stone]]()
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
@@ -26,6 +27,35 @@ class GameScene: SKScene {
         gameboard.name = "board"
         gameboard.zPosition = 2
         addChild(gameboard)
+        
+        // Setting up constant for positioning
+        let offsetX = -280
+        let offsetY = -281
+        let StoneSize = 80
+        
+        for row in 0 ..< 8 {
+            
+            var colArray = [Stone]()
+            
+            for col in 0 ..< 8{
+                let stone = Stone(color: UIColor.clear,size: CGSize(width: StoneSize, height: StoneSize ))
+            
+                stone.position = CGPoint(x: offsetX + (col * StoneSize), y: offsetY + (col * StoneSize))
+                
+                stone.row = row
+                stone.col = col
+                
+                gameboard.addChild(stone)
+                colArray.append(stone)
+            }
+            rows.append(colArray)
+        }
+        
+        rows[4][3].setPlayer(.white)
+        rows[4][4].setPlayer(.black)
+        rows[3][4].setPlayer(.white)
+        rows[3][3].setPlayer(.black)
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
--- Flip/Stone.swift
+++ Flip/Stone.swift
@@ -11,4 +11,27 @@ import SpriteKit
 
 class Stone: SKSpriteNode {
     
+   
+    
+    static let thinkingTexture = SKTexture(imageNamed: "thinking")
+    static let whiteTexture = SKTexture(imageNamed: "white")
+    static let blackTexture = SKTexture(imageNamed: "black")
+    
+    func setPlayer(_ player: StoneColor){
+        
+        if player == .white{
+            texture = Stone.whiteTexture
+        }
+        else if player == .black {
+            texture  = Stone.blackTexture
+        }
+        else if player == .choice {
+            texture = Stone.thinkingTexture
+        }
+    }
+
+    
+    var row = 0
+    var col = 0
+    
 }
